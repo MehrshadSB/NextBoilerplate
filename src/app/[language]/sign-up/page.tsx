9f6ae1a07ecf2d6fbd6dd0ca543c579c3aceef80
MehrshadSB <mehrshadsaebi.14@gmail.com>
2025-05-10 20:35:51 +0330

    Start

---
+++ src/app/[language]/sign-up/page.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import SignUp from "./page-content";
+import { getServerTranslation } from "@/services/i18n";
+import { redirect } from "next/navigation";
+import { IS_SIGN_UP_ENABLED } from "@/services/auth/config";
+import useLanguage from "@/services/i18n/use-language";
+
+type Props = {
+  params: Promise<{ language: string; slug: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+export async function generateMetadata(props: Props): Promise<Metadata> {
+  const params = await props.params;
+  const { t } = await getServerTranslation(params.language, "sign-up");
+
+  return {
+    title: t("title"),
+  };
+}
+
+export default function SignUpPage() {
+  if (!IS_SIGN_UP_ENABLED) {
+    return redirect("/");
+  }
+
+  return <SignUp />;
+}
